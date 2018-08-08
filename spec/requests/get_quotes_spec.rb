1cd3f701fe1fd14596bfc82d764de61be783f9ba
James Turello <jturello@yahoo.com>
2018-08-07 22:18:40 -0700

    add quotes index controller (hardcoded) & test

---
+++ spec/requests/get_quotes_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "returns all quotes", :type => :request do
+  let!(:size) {1} 
+  # let!(:quotes) {create_list(:quote, size)}
+
+  before { get '/quotes' }
+
+  it 'returns all quotes' do
+    expect(JSON.parse(response.body).size).to eq(size)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+
+end
