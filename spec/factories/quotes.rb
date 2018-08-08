5aafd3e25d6af8692b7492f56399ae6e2ebdb24e
James Turello <jturello@yahoo.com>
2018-08-07 22:57:12 -0700

    add factorybot & quote factory

---
+++ spec/factories/quotes.rb
@@ -0,0 +1,7 @@
+
+FactoryBot.define do
+  factory :quote do
+    content "Sample quote"
+    author  "sample author"
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,6 +54,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # config for FactoryBot factories
+  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
